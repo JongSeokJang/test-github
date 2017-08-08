4a2c8e8a57cde1d2a8809c9d6cf610ed7f495023
JongSeok Jang <whdtjr321@gmail.com>
2017-08-08 15:21:40 +0900

    init commit

--- Calculator.java
+++ Calculator.java
@@ -3,23 +3,6 @@ package com.mycompany.test;
 public class Calculator {
 
 	public static void main(String[] args) {
-        int a = 4;
-		int b = 2;
-		System.out.println(sum(a,b));
-        System.out.println(minus(a,b));
-        System.out.println(multi(a,b));
-	}
-
-    private static int sum(int a, int b){
-        return a + b;
-    }
-
-    private static int minus(int a, int b) {
-		return a - b;
-	}
-
-    private static int multi(int a, int b) {
-		return a * b;
 	}
 
 }
